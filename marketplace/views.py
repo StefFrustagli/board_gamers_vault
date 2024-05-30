15e58347d3dc5af0e03c9969074dc0804b8b45d4
StefFrustagli <stefania.frustagli@outlook.it>
2024-05-30 17:55:31 +0100

    FEAT: Implement search functionality
    This commit ensures the correct function of the search bar.
    It introduces these changes:
    - Implemented search form in the base template;
    - Added URL routing for search results;
    - Created view to handle search queries;
    - Updated games list template to display search results.
    - Tested to ensure search functionality works correctly.

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import Game
 
 # Create your views here.
@@ -10,10 +13,24 @@ def games_list(request):
     # 'games' is the name of the variable that holds
     # the data you retrieve from the database
     # in your view function
+    query = None
+
+    if request.GET:
+        if "q" in request.GET:
+            query = request.GET["q"]
+            if not query:
+                messages.error(request, "Please enter a search criteria.")
+                return redirect(reverse("games"))
+
+            queries = Q(title__icontains=query) | Q(description__icontains=query)
+            # i makes the case insensitive
+            games = games.filter(queries)
+
     context = {
         "games_list": games,  # 'games_list' is the name of the key in the
-                              # dictionary (context) that you pass to the 
-                              # template. The template uses this key  
+        # dictionary (context) that you pass to the
+        # template. The template uses this key
+        "search_term": query,
     }
     return render(request, "marketplace/games_list.html", context)
 
@@ -22,7 +39,6 @@ def game_detail(request, game_id):
     """A view to show individual product details"""
     game = get_object_or_404(Game, id=game_id)  # primary key lookup
     context = {
-        "game": game,  
-
+        "game": game,
     }
     return render(request, "marketplace/game_detail.html", context)
