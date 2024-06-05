c8af56af9dc27254b511efd3c193d3893e87f964
StefFrustagli <stefania.frustagli@outlook.it>
2024-06-06 00:40:13 +0100

    FEAT: Create checkout app with Order and OrderLineItem models
    This commit introduces the new checkout application and its associated models, Order and OrderLineItem. These models handle the functionality related to user orders, including the storage of order details and line items, order number generation, and total calculation.
    
    Changes include:
    - Creation of the checkout app;
    - Addition of the Order model, capturing order details such as full name, email, address, and total amounts;
    - Addition of the OrderLineItem model, capturing individual line items within an order;
    - Methods for generating unique order numbers, updating order totals, and managing line item totals.

--- board_gamers_vault/settings.py
+++ board_gamers_vault/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     "home",
     "marketplace",
     "bag",
+    "checkout",
 ]
 
 MIDDLEWARE = [
---
+++ checkout/models.py
@@ -0,0 +1,127 @@
+import uuid
+
+from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+
+from marketplace.models import Game
+
+# Create your models here.
+
+class Order(models.Model):
+    """
+    A model representing an order in the system.
+    """
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    country = models.CharField(max_length=40, null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    # Date and time when the order was created
+    date = models.DateTimeField(auto_now_add=True)
+    delivery_cost = models.DecimalField(
+        max_digits=6, decimal_places=2, null=False, default=0
+    )
+    order_total = models.DecimalField(
+        max_digits=10, decimal_places=2, null=False, default=0
+    )
+    grand_total = models.DecimalField(
+        max_digits=10, decimal_places=2, null=False, default=0
+    )
+
+    def _generate_order_number(self):
+        """
+        Generate a random, unique order number using UUID.
+
+        Returns:
+            str: A unique order number in hexadecimal format.
+        """
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+        """
+        Update grand total each time a line item is added,
+        accounting for delivery costs.
+
+        The method recalculates the order total by summing up
+        all line item totals. It then calculates the delivery cost
+        based on the order total and updates the grand total.
+        """
+        self.order_total = self.lineitems.aggregate(Sum("lineitem_total"))[
+            "lineitem_total__sum"
+        ]
+        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+            self.delivery_cost = (
+                self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+            )
+        else:
+            self.delivery_cost = 0
+        self.grand_total = self.order_total + self.delivery_cost
+        self.save()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        if it hasn't been set already.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        """
+        String representation of the order.
+
+        Returns:
+            str: The order number.
+        """
+        return self.order_number
+
+
+class OrderLineItem(models.Model):
+    """
+    A model representing an individual line item in an order.
+    """
+    order = models.ForeignKey(
+        Order,
+        null=False,
+        blank=False,
+        on_delete=models.CASCADE,
+        related_name="lineitems",
+    )
+    game = models.ForeignKey(
+        Game, null=False, blank=False, on_delete=models.CASCADE
+    )
+    lineitem_total = models.DecimalField(
+        max_digits=6, decimal_places=2, null=False, blank=False, editable=False
+    )
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the lineitem total
+        and update the order total.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+        """
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        """
+        String representation of the line item.
+
+        Returns:
+            str: A formatted string with the SKU and order number.
+        """
+        return f"SKU {self.product.sku} on order {self.order.order_number}"
