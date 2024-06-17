e896f9fb04e70fe656d2d2550875649ff3cc1291
StefFrustagli <stefania.frustagli@outlook.it>
2024-06-17 11:38:22 +0100

    ONGOING ISSUE: Set Debug to True and try to fix issue with deployment
    This commit tries to fix issue with deployment

--- board_gamers_vault/settings.py
+++ board_gamers_vault/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hab)(_b#t8sc%lnnp6ob!ep^@u1sab1vekqi@!r*c+m39+cvhj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "8000-steffrustag-boardgamers-wgstn1wvdge.ws-eu114.gitpod.io",
