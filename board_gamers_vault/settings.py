be05501e9259aa9cd88eda7e92dcd26284210ba5
StefFrustagli <stefania.frustagli@outlook.it>
2024-06-18 09:29:52 +0100

    CONFIG: Add secret key to environment configuration
    This commit includes the following changes:
    - Added SECRET_KEY to env.py;
    - Updated application settings to load the secret key from environment variables;
    - Ensures secure management of secret keys and separates configuration from code.

--- board_gamers_vault/settings.py
+++ board_gamers_vault/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hab)(_b#t8sc%lnnp6ob!ep^@u1sab1vekqi@!r*c+m39+cvhj'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
